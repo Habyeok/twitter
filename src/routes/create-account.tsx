af6ad4f8385bf7cc1d38946076971fe970ad96da
Habyeok <earnest_hb@naver.com>
2023-10-31 23:51:42 +0900

    wip: create account function 로직 추가

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,6 +1,8 @@
-import { setLogLevel } from "firebase/app";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { styled } from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -43,6 +45,7 @@ const Error = styled.span`
 `;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -58,12 +61,21 @@ export default function CreateAccount() {
       setPassword(value)
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
     try {
-      // create an account
-      // set the name of the user.
-      // redirect to the home page
+      setIsLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth, 
+        email, 
+        password
+      );
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/");
     } catch(e){
       // setError
     } finally {
@@ -74,7 +86,7 @@ export default function CreateAccount() {
   }
   return (
     <Wrapper>
-      <Title>Log into 😺</Title>
+      <Title>Join 😺</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
